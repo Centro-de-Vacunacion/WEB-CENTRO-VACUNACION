1b7ac00c5a49a304dda9824ec17792a5001da0e6
Jefry <jnavass@est.ups.edu.ec>
2021-07-22 18:20:07 -0500

    verificacion del usuario

---
+++ control/config.js
@@ -0,0 +1,14 @@
+calcularEdad = async(fecha) => {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
+
+module.exports = { calcularEdad }
---
+++ hbs/helpers.js
@@ -0,0 +1,30 @@
+const hbs = require("hbs");
+// Establecer el helper
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('capitalizar', (texto) => {
+    let palabras = texto.split(' ');
+    palabras.forEach((palabra, index) => {
+        palabras[index] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+    return palabras.join(' ');
+});
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+hbs.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+});
